7d2068bcdf02a3702d98c132d0042d23a990278b
Alex Brinkman <abrink@commercekitchen.com>
2015-10-07 13:43:05 -0600

    Merge branch 'setup_authentication'

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20151007165045) do
-=======
-ActiveRecord::Schema.define(version: 20151006223358) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "first_name"
@@ -24,7 +21,6 @@ ActiveRecord::Schema.define(version: 20151006223358) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
->>>>>>> master
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -46,14 +42,11 @@ ActiveRecord::Schema.define(version: 20151006223358) do
     t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-<<<<<<< HEAD
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-=======
     t.integer  "profile_id"
->>>>>>> master
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
