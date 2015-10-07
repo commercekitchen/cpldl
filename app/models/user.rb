efb5c6efd7f955576878fb378129ae0ba50b7246
coreydavis <corey@commercekitchen.com>
2015-10-07 10:47:20 -0600

    [CHG] Move devise forms to single view Create UserAccounts Controller, add devise mappings to user_accounts_ helper, and render both devise forms (registration and session) to single view /user_accounts/new.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_one :profile, dependent: :destroy
   accepts_nested_attributes_for :profile
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'home#index'
-
   resource :profile, only: [:show, :update]
-
+  get 'user_accounts/new'
 end
