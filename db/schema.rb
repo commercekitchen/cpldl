58d4dff5c46ec9e797baceaa0b525c3005679b42
coreydavis <corey@commercekitchen.com>
2015-10-07 12:22:23 -0600

    [CHG] Add confirmable module to devise Adds in the confirmatble module and configures email for development with devise. Currently using PLA PO.org credentials, will need to change this over once the domain is provisioned.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006221749) do
+ActiveRecord::Schema.define(version: 20151007165045) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,8 +26,13 @@ ActiveRecord::Schema.define(version: 20151006221749) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
