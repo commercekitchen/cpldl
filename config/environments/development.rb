a3bd8e12c3860260a506b68e47725b3dcddbb901
coreydavis <corey@commercekitchen.com>
2015-10-07 12:22:23 -0600

    [CHG] Add confirmable module to devise
    Adds in the confirmatble module and configures email for development
    with devise. Currently using PLA PO.org credentials, will need to
    change this over once the domain is provisioned.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,15 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # => to send email from local host
+  config.action_mailer.default_url_options = { :host => 'localhost', port: 3000, only_path: false }
+
+  # => to be informed of delivery errors
+  config.action_mailer.raise_delivery_errors = true
+
+  # => to send email over network
+  # => this is temporary (we will need to change this)
+  config.action_mailer.delivery_method = :smtp
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006223358) do
+ActiveRecord::Schema.define(version: 20151007165045) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "first_name"
@@ -36,8 +36,13 @@ ActiveRecord::Schema.define(version: 20151006223358) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "profile_id"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
