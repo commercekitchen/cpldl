aed6447e253f8b78cfcc8a57a537d279563a60f0
coreydavis <corey@commercekitchen.com>
2015-10-07 13:48:38 -0600

    [FIX] Fix git misalignment Git and Git hub are out of alignment, this is intented to re-align them.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
+  
   devise_for :users
-  get 'user_accounts/new'
+    get 'user_accounts/new'
+  
   resource :profile, only: [:show, :update]
 end
