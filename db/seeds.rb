b9c097d3cf65eb614d5c8abf6c0d5d0952ba8a47
coreydavis <corey@commercekitchen.com>
2015-10-08 12:00:43 -0600

    [TOOL] Add temp super user acct. to seeds file Adding a temporary super user account to seeds file, this is for dev; will be modified once it becomes relevant.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# => super user account
+super_user = User.create(email: "super@commercekitchen.com",
+                      password: "superM@n",
+                  confirmed_at: Time.now)
+
+super_profile = Profile.create(first_name: "Super",
+                                last_name: "User",
+                                 zip_code: "80206",
+                                  user_id: super_user.id)
+
+super_user.update(profile_id: super_profile.id)
+super_user.add_role(:super)
