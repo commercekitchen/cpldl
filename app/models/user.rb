7137e0ded914f7076016bb5d82ff9b6d897b8f55
coreydavis <corey@commercekitchen.com>
2015-10-08 10:31:24 -0600

    [TOOL] Configure RSpec Initial tests include: user & profile factories, profile controller spec, user controller spec, user feature specs (sign up & log in).

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
   has_one :profile, dependent: :destroy
+
   accepts_nested_attributes_for :profile
 end
