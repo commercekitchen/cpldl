abe1d7728cbc3232cbeb50104331b60d0ac53cfd
coreydavis <corey@commercekitchen.com>
2015-10-08 11:16:31 -0600

    [TOOL] Configure RSpec with initial tests Adds initial tests for RSpec: - User features (sign up and log in) - User controller - Profile controller - Factorys (User / Profile)

---
+++ spec/features/user/user_logs_in_spec.rb
@@ -0,0 +1,42 @@
+require "feature_helper"
+
+feature "User logs in" do
+
+  scenario "with valid email and password" do
+    user ||= FactoryGirl.create(:user)
+    log_in_with user.email, user.password
+
+    expect(page).to have_content("Signed in successfully.")
+  end
+
+  scenario "with invalid or blank email" do
+    log_in_with "", "password"
+    expect(page).to have_content("Invalid email or password.")
+
+    log_in_with "not@real.com", "password"
+    expect(page).to have_content("Invalid email or password.")
+
+  end
+
+  scenario "with blank password" do
+    log_in_with "valid@example.com", ""
+    expect(page).to have_content("Invalid email or password.")
+
+    log_in_with "valid@example.com", "no correct pwd"
+    expect(page).to have_content("Invalid email or password.")
+  end
+
+  scenario "with unconfirmed email" do
+    user ||= FactoryGirl.create(:unconfirmed_user)
+    log_in_with user.email, user.password
+    expect(page).to have_content("You have to confirm your email address before continuing.")
+  end
+
+  def log_in_with(email, password)
+    visit user_account_path
+    find("#login_email").set(email)
+    find("#login_password").set(password)
+    fill_in "Password confirmation", with: password
+    click_button "Log in"
+  end
+end
