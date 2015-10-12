b0e04c2438e64d28fe3b1ea987480712773c9513
Alex Brinkman <abrink@commercekitchen.com>
2015-10-12 15:05:29 -0600

    [CHG] Change password fields to show text
    To make the site easier to use, we're showing the password text as
    it is being typed.  We don't consider this to be a security violation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
-  
+
   devise_for :users
-    get 'user_accounts/new', as: :user_account
-  
+  get 'user_accounts/new', as: :user_account
+
   resource :profile, only: [:show, :update]
 end
--- spec/controllers/profile_controller_spec.rb
+++ spec/controllers/profile_controller_spec.rb
@@ -2,42 +2,49 @@ require "rails_helper"
 
 describe ProfilesController do
 
-  describe "GET #show" do
-
+  describe "#show" do
     context "when logged in" do
-
-      before(:each) do
-        @user = FactoryGirl.create(:user)
-        sign_in @user
-      end
-
-      it ".show" do
+      it "should show the user's profile information" do
+        user = FactoryGirl.create(:user)
+        sign_in user
         get :show
         expect(response).to have_http_status(:success)
       end
-
-      it ".update" do
-        put :update, id: @user.profile,
-        user: { email: @user.email, password: @user.password, password_confirmation: @user.password,
-          profile_attributes: { first_name: "Robby", last_name: "Rrown", zip_code: "12345" } },
-          authenticity_token: set_authenticity_token
-
-        @user.reload
-        expect(@user.profile.first_name).to eq("Robby")
-        expect(@user.profile.last_name).to eq("Rrown")
-        expect(@user.profile.zip_code).to eq("12345")
-      end
-
     end
 
     context "when logged out" do
-
-      it "redirects" do
+      it "should redirect any action to login page" do
         get :show
         expect(response).to have_http_status(:redirect)
         expect(response).to redirect_to(user_session_path)
       end
+    end
+  end
 
+  describe "#update" do
+
+    context "when logged in" do
+      it "allows the user to update thier profile information" do
+        user = FactoryGirl.create(:user)
+        sign_in user
+        put :update, id: user.profile,
+        user: { email: user.email, password: user.password, password_confirmation: user.password,
+          profile_attributes: { first_name: "Robby", last_name: "Rrown", zip_code: "12345" } },
+          authenticity_token: set_authenticity_token
+
+        user.reload
+        expect(user.profile.first_name).to eq("Robby")
+        expect(user.profile.last_name).to eq("Rrown")
+        expect(user.profile.zip_code).to eq("12345")
+      end
+    end
+
+    context "when logged out" do
+      it "should redirect any action to login page" do
+        put :update
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(user_session_path)
+      end
     end
 
   end
