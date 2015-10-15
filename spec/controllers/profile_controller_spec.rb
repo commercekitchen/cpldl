43c02861c35eef49da9557d1c3cd1e0629540bef
Alex Brinkman <abrink@commercekitchen.com>
2015-10-14 22:18:15 -0600

    [CHG] Add more test coverage around profiles
    We did not have coverage around just changing an email address, now
    we do.

--- spec/controllers/profile_controller_spec.rb
+++ spec/controllers/profile_controller_spec.rb
@@ -25,25 +25,37 @@ describe ProfilesController do
 
     context "when logged in" do
 
-      it "allows the user to update thier profile information" do
-        user = FactoryGirl.create(:user)
-        sign_in user
-        put :update, id: user.profile,
-        user: { email: user.email, password: user.password, password_confirmation: user.password,
+      before(:each) do
+        @user = FactoryGirl.create(:user)
+        sign_in @user
+      end
+
+      it "allows the user to update their profile information" do
+        put :update, id: @user.profile,
+        user: { email: @user.email, password: @user.password, password_confirmation: @user.password,
           profile_attributes: { first_name: "Robby", last_name: "Rrown", zip_code: "12345" } },
           authenticity_token: set_authenticity_token
 
-        user.reload
-        expect(user.profile.first_name).to eq("Robby")
-        expect(user.profile.last_name).to eq("Rrown")
-        expect(user.profile.zip_code).to eq("12345")
+        @user.reload
+        expect(@user.profile.first_name).to eq("Robby")
+        expect(@user.profile.last_name).to eq("Rrown")
+        expect(@user.profile.zip_code).to eq("12345")
+      end
+
+      it "allows the user to update just their email" do
+        put :update, id: @user.profile,
+        user: { email: "new@commercekitchen.com", password: "", password_confirmation: "",
+          profile_attributes: { first_name: "", last_name: "", zip_code: "" } },
+          authenticity_token: set_authenticity_token
+        @user.reload
+        expect(response).to redirect_to(profile_path)
+        expect(flash[:notice]).to be_present
+        expect(@user.unconfirmed_email).to eq("new@commercekitchen.com")
       end
 
       it "should not allow invalid user information" do
-        user = FactoryGirl.create(:user)
-        sign_in user
-        put :update, id: user.profile,
-        user: { email: user.email, password: user.password, password_confirmation: "something else",
+        put :update, id: @user.profile,
+        user: { email: @user.email, password: @user.password, password_confirmation: "something else",
           profile_attributes: { first_name: "", last_name: "", zip_code: "" } },
           authenticity_token: set_authenticity_token
         expect(assigns(:user).errors.any?).to be true
