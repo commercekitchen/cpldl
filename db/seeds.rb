f3f1dc0b09ea2cdd4572dd69185e7f2588fc50aa
Alex Brinkman <abrink@commercekitchen.com>
2015-10-19 11:43:27 -0600

    [REFAC] Update seeds/create sample data rake task
    The seeds file should be real, valid data, not sample data.
    This change pulls out all faker data for use in development into its
    own rake task, leaving the seeds file cleaner and suitable for a new
    site build.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,50 +1,46 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-# => super user account
-# => CHANGE pwd for production!!!
-super_user = User.create(email: "super@cpl.com", password: "password", confirmed_at: Time.zone.now)
-super_profile = Profile.create(first_name: "Super", last_name: "User", zip_code: "80206", user_id: super_user.id)
-super_user.update(profile_id: super_profile.id)
-super_user.add_role(:admin)
+admin_user = User.create(email: "admin@commercekitchen.com", password: "ChangeMe!", confirmed_at: Time.zone.now)
+admin_profile = Profile.create(first_name: "Super", last_name: "User", zip_code: "80206", user_id: admin_user.id)
+admin_user.update(profile_id: admin_profile.id)
+admin_user.add_role(:admin)
+puts "Admin User Created - Username: #{admin_user.email}, Password: password"
 
-puts "Super User Created - Username: #{super_user.email}, Password: password"
+regular_user = User.create(email: "alex@commercekitchen.com", password: "asdfasdf", confirmed_at: Time.zone.now)
+admin_profile = Profile.create(first_name: "Alex", last_name: "Brinkman", zip_code: "80209", user_id: regular_user.id)
+regular_user.update(profile_id: admin_profile.id)
+puts "Regular User Created - Username: #{regular_user.email}, Password: password"
 
-# => temporary topics for development
-Topic.create(title: "Road Warriors")
-Topic.create(title: "ThunderDome")
-Topic.create(title: "War Boys")
-Topic.create(title: "Gas Town")
+Topic.create(title: "Computers")
+Topic.create(title: "Internet")
 puts "#{Topic.count} topics created."
 
-# => fake languages
-Language.create(name: "Klingon")
-Language.create(name: "Orc")
-Language.create(name: "Zanzibarzian")
+Language.create(name: "English")
+Language.create(name: "Spanish")
 puts "#{Language.count} languages created."
 
 # => Temporary courses for development
-6.times do
+6.times do |i|
   Course.create!(
-    title: Faker::Company.name,
-    seo_page_title: Faker::Company.bs,
-    meta_desc: Faker::Company.bs,
-    summary: Faker::Lorem.sentence(5),
-    description: Faker::Lorem.paragraph(3),
-    contributor: Faker.name,
-    pub_status: %w(P D T).sample,
-    language_id: Language.all.sample.id,
-    level: %w(Beginner Intermediate Advanced).sample
+    title: "Sample Course #{i+1}",
+    seo_page_title: "Sample Course #{i+1}",
+    meta_desc: "Meta Description for Sample Course #{i+1}",
+    summary: "Summary for Sample Course #{i+1}",
+    description: "Description for Sample Course #{i+1}",
+    contributor: "John Doe",
+    pub_status: "P",
+    language_id: Language.first.id,
+    level: "Beginner"
   )
 end
 puts "#{Course.count} courses created."
 
 Course.all.each do |c|
-  c.topics << Topic.all.sample
+  c.topics << Topic.first
   c.lessons << Lesson.create(title: "Lesson 1", description: "Lesson A description", duration: 90, order: 1)
   c.lessons << Lesson.create(title: "Lesson 2", description: "Lesson B description", duration: 120, order: 2)
   c.lessons << Lesson.create(title: "Lesson 3", description: "Lesson B description", duration: 80, order: 3)
   c.lessons << Lesson.create(title: "Lesson 4", description: "Lesson B description", duration: 40, order: 4)
   c.save
 end
-puts "courses updated with topics, languages, and lessons."
---
+++ lib/tasks/load_sample_data.rake
@@ -0,0 +1,56 @@
+namespace :db do
+  desc "Load sample data"
+  task load_sample_data: :environment do
+    puts "Loading sample data..."
+
+    # => super user account
+    # => CHANGE pwd for production!!!
+    super_user = User.create(email: "super@cpl.com", password: "password", confirmed_at: Time.zone.now)
+    super_profile = Profile.create(first_name: "Super", last_name: "User", zip_code: "80206", user_id: super_user.id)
+    super_user.update(profile_id: super_profile.id)
+    super_user.add_role(:admin)
+
+    puts "Super User Created - Username: #{super_user.email}, Password: password"
+
+    # => temporary topics for development
+    Topic.create(title: "Road Warriors")
+    Topic.create(title: "ThunderDome")
+    Topic.create(title: "War Boys")
+    Topic.create(title: "Gas Town")
+    puts "#{Topic.count} topics created."
+
+    # => fake languages
+    Language.create(name: "Klingon")
+    Language.create(name: "Orc")
+    Language.create(name: "Zanzibarzian")
+    puts "#{Language.count} languages created."
+
+    # => Temporary courses for development
+    6.times do
+      Course.create!(
+        title: Faker::Company.name,
+        seo_page_title: Faker::Company.bs,
+        meta_desc: Faker::Company.bs,
+        summary: Faker::Lorem.sentence(5),
+        description: Faker::Lorem.paragraph(3),
+        contributor: Faker.name,
+        pub_status: %w(P D T).sample,
+        language_id: Language.all.sample.id,
+        level: %w(Beginner Intermediate Advanced).sample
+      )
+    end
+    puts "#{Course.count} courses created."
+
+    Course.all.each do |c|
+      c.topics << Topic.all.sample
+      c.lessons << Lesson.create(title: "Lesson 1", description: "Lesson A description", duration: 90, order: 1)
+      c.lessons << Lesson.create(title: "Lesson 2", description: "Lesson B description", duration: 120, order: 2)
+      c.lessons << Lesson.create(title: "Lesson 3", description: "Lesson B description", duration: 80, order: 3)
+      c.lessons << Lesson.create(title: "Lesson 4", description: "Lesson B description", duration: 40, order: 4)
+      c.save
+    end
+    puts "courses updated with topics, languages, and lessons."
+
+    puts "Complete."
+  end
+end
