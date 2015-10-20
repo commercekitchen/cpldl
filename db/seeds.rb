d3d670e7565f52633462c759356e8f0b4ac923be
Alex Brinkman <abrink@commercekitchen.com>
2015-10-20 15:54:45 -0600

    [CHG] Update styling of course lesson blocks
    On the view course page, all of the lessons are shown in a block
    component.  This commit cleans up the styling a bit.

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,12 +22,28 @@ puts "#{Language.count} languages created."
 
 # => Temporary courses for development
 6.times do |i|
+  description = <<-DESCRIPTION
+    Description for Sample Course #{i+1}. At vero eos et accusamus et iusto odio
+    dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
+    atque corrupti quos dolores et quas molestias excepturi
+    sint occaecati cupiditate non provident, similique sunt
+    in culpa qui officia deserunt mollitia animi,
+    id est laborum et dolorum fuga. Et harum quidem rerum
+    facilis est et expedita distinctio. Nam libero tempore,
+    cum soluta nobis est eligendi optio cumque nihil impedit
+    quo minus id quod maxime placeat facere possimus, omnis
+    voluptas assumenda est, omnis dolor repellendus. Temporibus
+    autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet
+    ut et voluptates repudiandae sint et molestiae non recusandae.
+    Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+    voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat..
+  DESCRIPTION
   Course.create!(
     title: "Sample Course #{i+1}",
     seo_page_title: "Sample Course #{i+1}",
     meta_desc: "Meta Description for Sample Course #{i+1}",
     summary: "Summary for Sample Course #{i+1}",
-    description: "Description for Sample Course #{i+1}",
+    description: description,
     contributor: "John Doe",
     pub_status: "P",
     language_id: Language.first.id,
