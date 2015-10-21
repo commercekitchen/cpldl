f60b1839a02936a7b759b365275d936e58f13335
Alex Brinkman <abrink@commercekitchen.com>
2015-10-21 14:19:44 -0600

    [TOOL] Add sendgrid initializer for emails

---
+++ config/initializers/sendgrid.rb
@@ -0,0 +1,19 @@
+# Initialize ActionMailer settings for sendgrid
+
+login = Rails.application.secrets.sendgrid_login
+password = Rails.application.secrets.sendgrid_password
+domain = Rails.application.secrets.sendgrid_domain || "cpldl-integapp-01.do.lark-it.com"
+
+if login.nil? and password.nil?
+  abort('Please ensure the sendgrid_login and sendgrid_password are defined in secrets.yml')
+else
+  ActionMailer::Base.smtp_settings = {
+    :user_name => login,
+    :password => password,
+    :domain => domain,
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
