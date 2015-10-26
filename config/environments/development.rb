3b5a43689daf1c3b6a25d93593be2f9ee6740135
Alex Brinkman <abrink@commercekitchen.com>
2015-10-26 10:45:37 -0600

    [TOOL] Configure Bullet gem for N+1 query detect

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,16 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  # Bullet config
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   Paperclip.options[:command_path] = "/usr/local/bin/"
+
 end
