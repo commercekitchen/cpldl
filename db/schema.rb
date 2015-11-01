33d7807f98ba245ef58d6ff7d85529ede94659af
Alex Brinkman <abrink@commercekitchen.com>
2015-10-31 18:21:08 -0600

    [CHG] Remove CKEditor assets table
    The CKEditor gem was removed previously, but this table was left
    behind inadvertantly.  This commit cleans that up.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028205201) do
+ActiveRecord::Schema.define(version: 20151101001943) do
 
   create_table "attachments", force: :cascade do |t|
     t.integer  "course_id"
@@ -24,22 +24,6 @@ ActiveRecord::Schema.define(version: 20151028205201) do
     t.datetime "document_updated_at"
     t.string   "doc_type"
   end
-
-  create_table "ckeditor_assets", force: :cascade do |t|
-    t.string   "data_file_name",               null: false
-    t.string   "data_content_type"
-    t.integer  "data_file_size"
-    t.integer  "assetable_id"
-    t.string   "assetable_type",    limit: 30
-    t.string   "type",              limit: 30
-    t.integer  "width"
-    t.integer  "height"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-  end
-
-  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
-  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
 
   create_table "completed_lessons", force: :cascade do |t|
     t.integer  "course_progress_id"
