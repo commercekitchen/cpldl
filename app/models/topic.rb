b6801dfcc6073425dc07017aa9f5bd8f0eeeac10
Jared Koumentis <jared@koumentis.com>
2015-11-03 16:02:02 -0700

    [CHG] Updates course pg_search.
    This is needed to update all the courses that have a given topic, if
    the topic is updated. This way, the PgSearch::Document for each course
    stays in sync.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ActiveRecord::Base
   has_many :course_topics
-  has_many :courses, through: :course_topics
+  has_many :courses, through: :course_topics, touch: true
 
   validates :title, presence: true
 end
