423332e6eb00b60660be169961dc130593ce7688
Jared Koumentis <jared@koumentis.com>
2015-11-05 12:40:44 -0700

    [TOOL] Add annotations

--- app/models/completed_lesson.rb
+++ app/models/completed_lesson.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: completed_lessons
+#
+#  id                 :integer          not null, primary key
+#  course_progress_id :integer
+#  lesson_id          :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 class CompletedLesson < ActiveRecord::Base
   belongs_to :course_progress
 end
--- spec/models/course_progress_spec.rb
+++ spec/models/course_progress_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: course_progresses
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  course_id    :integer
+#  started_at   :datetime
+#  completed_at :datetime
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  tracked      :boolean          default(FALSE)
+#
+
 require "rails_helper"
 
 describe CourseProgress do
