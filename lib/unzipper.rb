79015d0299da6665d2eff8b7aa0af2dc90da97a9
coreydavis <corey@commercekitchen.com>
2015-11-05 13:08:09 -0700

    WIP

--- lib/unzipper.rb
+++ lib/unzipper.rb
@@ -1,18 +1,16 @@
 class Unzipper
   def initialize(asl_package)
     @file = asl_package
-    package_prepper
+    package_prepper unless @file.blank?
   end
 
   def package_prepper
-    unless @file.nil?
-      file_name   = @file.instance.story_line_file_name.chomp(".zip")
-      import_path = "public/system/lessons/story_lines/#{@file.instance.id}"
-      public_path = "public/storylines/#{@file.instance.id}"
-      zip_file    = File.join(Rails.root, "#{import_path}/original/#{file_name}.zip")
-      dest_path   = File.join(Rails.root, "#{public_path}/#{file_name}")
-      unzip_asl(zip_file, dest_path)
-    end
+    file_name   = @file.instance.story_line_file_name.chomp(".zip")
+    import_path = "public/system/lessons/story_lines/#{@file.instance.id}"
+    public_path = "public/storylines/#{@file.instance.id}"
+    zip_file    = File.join(Rails.root, "#{import_path}/original/#{file_name}.zip")
+    dest_path   = File.join(Rails.root, "#{public_path}/#{file_name}")
+    unzip_asl(zip_file, dest_path)
   end
 
   def unzip_asl(zip_file, dest_path)
