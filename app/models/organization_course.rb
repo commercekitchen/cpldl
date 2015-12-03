1690a0a46b5264ddd977144bb53b594d47317adb
Jared Koumentis <jared@koumentis.com>
2015-12-02 21:28:02 -0700

    [TEST] Adds organization factory

--- app/models/organization_course.rb
+++ app/models/organization_course.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: organization_courses
+#
+#  id              :integer          not null, primary key
+#  organization_id :integer
+#  course_id       :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class OrganizationCourse < ActiveRecord::Base
   belongs_to :organization
   belongs_to :course
