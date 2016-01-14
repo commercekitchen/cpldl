312fdc18795c54b8a78db4c87d1c14c600c40edf
Jared Koumentis <jared@commercekitchen.com>
2016-01-14 02:28:39 -0700

    [FEAT] Only shows courses that are for the current subdomain

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,9 @@ class HomeController < ApplicationController
     spanish_id = Language.find_by_name("Spanish").id || 2
     case I18n.locale
     when :es
-      @courses = Course.includes(:lessons).where(pub_status: "P", language_id: spanish_id)
+      @courses = Course.includes(:lessons).where(pub_status: "P", language_id: spanish_id).where_exists(:organization, subdomain: request.subdomain)
     else
-      @courses = Course.includes(:lessons).where(pub_status: "P", language_id: english_id)
+      @courses = Course.includes(:lessons).where(pub_status: "P", language_id: english_id).where_exists(:organization, subdomain: request.subdomain)
     end
   end
 
