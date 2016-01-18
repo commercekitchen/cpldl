0ac99a9db6a680aa85b79672d6afb44be3f821d2
Jared Koumentis <jared@commercekitchen.com>
2016-01-18 12:51:56 -0700

    [TOOL] Correct the server domain

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ set :bundle_without, %w{integration staging test}.join(" ")
 
 set :branch, "master"
 
-server "cpldl-prodapp-01.do.lark-it.com",
+server "dl-prodapp-01.do.lark-it.com",
   user: fetch(:application),
   port: 1022,
   roles: %w{web app db},
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,7 @@ set :bundle_without, %w{integration production test}.join(" ")
 
 set :branch, "master"
 
-server "cpldl-stageapp-01.do.lark-it.com",
+server "dl-stageapp-01.do.lark-it.com",
   user: fetch(:application),
   port: 1022,
   roles: %w{web app db},
