dc2cf8ee70cc41cf37f0dc66185162f97e5cd30d
Jared Koumentis <jared@commercekitchen.com>
2016-01-22 12:05:24 -0700

    [CHG] Another force ssl attempt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.force_ssl = true
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
