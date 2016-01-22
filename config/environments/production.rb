33dda39135a1e56c168cd68e59236d4034bbf3f9
Jared Koumentis <jared@commercekitchen.com>
2016-01-22 12:19:53 -0700

    [CHG] Set host for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   config.force_ssl = true
-  
+
+  config.action_mailer.default_url_options = { host: "digitallearn.org" }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
