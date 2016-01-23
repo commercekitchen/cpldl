2a5dda80249caeb6984b22cab636148e916ffcd3
Jared Koumentis <jared@commercekitchen.com>
2016-01-23 02:08:20 -0700

    [FIX] Asset compilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg)
+# Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images")
