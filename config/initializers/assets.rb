1895a43a77d6d3401abffb35b9a5a8fd4a67b748
Jared Koumentis <jared@commercekitchen.com>
2016-01-23 04:02:19 -0700

    JS

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( courses.js jquery.simplyCountable.js )
 Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg)
 Rails.application.config.assets.paths << "#{Rails.root}/app/assets/fonts"
 Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
