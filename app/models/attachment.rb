1d01a8070e9bba4e4abb32566d414d8e3aaacfff
Jared Koumentis <jared@commercekitchen.com>
2016-01-26 16:39:07 -0700

    [CHG] Adds common office extensions to attachments.

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -18,8 +18,15 @@ class Attachment < ActiveRecord::Base
   belongs_to :course
   has_attached_file :document
 
-  validates_attachment_content_type :document, content_type: ["application/pdf", "text/plain"],
-                                                    message: ", Only PDF files are allowed."
+  validates_attachment_content_type :document, content_type: ["application/pdf",
+                                                              "text/plain",
+                                                              "application/vnd.ms-excel",
+                                                              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                                                              "application/vnd.ms-powerpoint",
+                                                              "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+                                                              "application/msword",
+                                                              "application/vnd.openxmlformats-officedocument.wordprocessingml.document"],
+                                                    message: ", Only PDF, Word, PowerPoint, or Excel files are allowed."
 
   validates :doc_type, allow_blank: true,
                          inclusion: { in: %w(supplemental post-course),
