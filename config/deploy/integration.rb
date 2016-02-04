2fad4f31c0fe5cf6c63d5ff8648aa9085fa55edb
Jared Koumentis <jared@commercekitchen.com>
2016-02-04 10:52:44 -0700

    [CHG] Point deploy at new integ server

--- config/deploy/integration.rb
+++ config/deploy/integration.rb
@@ -5,7 +5,7 @@ set :bundle_without, %w{production staging test}.join(" ")
 
 set :branch, "master"
 
-server "cpldl-integapp-01.do.lark-it.com",
+server "dl-integapp-01.do.lark-it.com",
   user: fetch(:application),
   port: 1022,
   roles: %w{web app db},
