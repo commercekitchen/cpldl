30e57a213bfeb6a7d8f5cf982dc0cd2c5b0acf7c
coreydavis <corey@commercekitchen.com>
2016-02-09 16:29:23 -0700

    [ REFAC ] refac invitation method

--- app/controllers/admin/invites_controller.rb
+++ app/controllers/admin/invites_controller.rb
@@ -5,8 +5,7 @@ module Admin
     def invite_resource
       user = User.invite!({email: params[:user][:email]}, current_user) do |u|
         u.skip_invitation = true
-        org_name = URI.parse(request.original_url).host.split(".")[0]
-        u.add_role(:admin, Organization.find_by(name: org_name))
+        u.add_role(:admin, Organization.find_by_subdomain(request.subdomain))
         u.deliver_invitation
       end
     end
