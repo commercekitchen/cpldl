f5f79834ca8f3d78acf7e4c32ee607d37f4edaeb
coreydavis <corey@commercekitchen.com>
2016-03-09 10:24:00 -0700

    [ CHG ] Remove un-needed commented code

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,8 +1,3 @@
-# class Ckeditor::Asset < ActiveRecord::Base
-#   include Ckeditor::Orm::ActiveRecord::AssetBase
-#   include Ckeditor::Backend::Paperclip
-# end
-
 module Ckeditor
   class Asset < ActiveRecord::Base
     include Ckeditor::Orm::ActiveRecord::AssetBase
