738ffb6a121503b38070e3e3a5f75537205f8fd2
coreydavis <corey@commercekitchen.com>
2016-03-21 12:56:30 -0600

    [ BUG ] Fix bug in lessons pub_status migration

--- db/migrate/20160315204732_add_pub_status_to_lessons.rb
+++ db/migrate/20160315204732_add_pub_status_to_lessons.rb
@@ -1,5 +1,14 @@
 class AddPubStatusToLessons < ActiveRecord::Migration
-  def change
+  def self.up
     add_column :lessons, :pub_status, :string
+
+    Lesson.all.each do |lesson|
+      lesson.pub_status = "P"
+      lesson.save
+    end
+  end
+
+  def self.down
+    remove_column :lessons, :pub_status, :string
   end
 end
