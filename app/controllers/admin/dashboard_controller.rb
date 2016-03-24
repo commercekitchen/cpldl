4ade72ad0ac7843c6e8c7058ed4e02b58606bb2b
coreydavis <corey@commercekitchen.com>
2016-03-24 15:21:31 -0600

    [ WIP ]

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -9,7 +9,7 @@ module Admin
     end
 
     def pages_index
-      @cms_pages = CmsPage.where(organization_id: Organization.find_by_subdomain(request.subdomain).id)
+      @cms_pages = CmsPage.where(organization_id: Organization.find_by_subdomain(request.subdomain).try(:id))
       render "admin/cms_pages/index", layout: "admin/base_with_sidebar"
     end
 
