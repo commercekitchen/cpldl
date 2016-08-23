b1b708cafce7567b6569801229d680a027e9e2e4
Chad Lundgren <chad.lundgren@gmail.com>
2016-08-23 12:05:43 -0600

    Library name upgrade (#163)
    * [CHG] Output Library Names in Completions Report
    
    Lookup library name rather than id
    Tests for library lookup, including new factory
    Removing un-needed ActiveRecord::Base class inheritance

---
+++ spec/factories/library_location.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: languages
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+FactoryGirl.define do
+  factory :library_location do
+    name "Back of the Yards"
+  end
+
+end
