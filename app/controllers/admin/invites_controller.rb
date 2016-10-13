d3390046d50b85d7858a11fcf288275834deb791
Desi McAdam <desi.mcadam@gmail.com>
2016-10-13 17:19:31 -0600

    Admin of www can manage organizations

--- app/controllers/admin/invites_controller.rb
+++ app/controllers/admin/invites_controller.rb
@@ -3,9 +3,11 @@ module Admin
     private
 
     def invite_resource
+      organization = Organization.find(params[:user][:organization_id]) || current_organization
       User.invite!({ email: params[:user][:email] }, current_user) do |u|
+        u.organization = organization
         u.skip_invitation = true
-        u.add_role(:admin, current_organization)
+        u.add_role(:admin, organization)
         u.deliver_invitation
       end
     end
---
+++ app/controllers/admin/organizations_controller.rb
@@ -0,0 +1,28 @@
+module Admin
+  class OrganizationsController < BaseController
+    def index
+      @organizations = Organization.all
+    end
+
+    def new
+      @organization = Organization.new
+    end
+
+    def create
+      @organization = Organization.new(organization_params)
+
+      if @organization.errors.any?
+        render :new
+      else @organization.save
+        redirect_to admin_organizations_path, notice: "Organization was successfully created."
+      end
+    end
+
+    private
+
+    def organization_params
+      params.require(:organization).permit(:name, :subdomain)
+    end
+
+  end
+end
