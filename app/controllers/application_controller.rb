564ea62cee25566088d1eea2631b2b5a5bfc6c9b
Tom Reis <tom@commercekitchen.com>
2016-11-16 11:58:54 -0700

    [BUG] Fix application controller redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -69,7 +69,7 @@ class ApplicationController < ActionController::Base
       profile_path
     elsif user.has_role?(:admin, current_organization)
       admin_dashboard_index_path
-    elsif current_organization.subdomain == "npl" && user.profile.program_location.blank?
+    elsif current_organization.subdomain == "npl" && user.program_location.blank?
       @profile = user.profile
       profile_path
     else
