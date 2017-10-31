eadd3214adb9e3981e868919e2140b4771eb8ffc
Alex Brinkman <abrink@commercekitchen.com>
2017-10-30 22:46:53 -0600

    [CHG] Bump version number

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :bundle_without, %w{production test}.join(" ")
 
-set :branch, "release-2.5.3"
+set :branch, "release-2.5.4"
 
 server "dl-stageapp-01.do.lark-it.com",
   user: fetch(:application),
