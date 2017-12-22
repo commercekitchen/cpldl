a9b375ca0240639634749df3eea43e4f23b7becd
Alex Brinkman <abrink@commercekitchen.com>
2017-12-21 19:26:08 -0700

    [CHG] Update Rails to 4.2.10
    This should be done anyway, but became urgent because of the security
    vulnerability in the previous verion of nokogiri used as a dependency
    in Rails 4.2.7.1

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@
 source 'https://rubygems.org'
 
 # Base set of gems
-gem 'rails', '4.2.7.1'
+gem 'rails', '4.2.10'
 gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :bundle_without, %w{production test}.join(" ")
 
-set :branch, "release-2.6.0"
+set :branch, "release-2.6.1"
 
 server "dl-stageapp-01.do.lark-it.com",
   user: fetch(:application),
