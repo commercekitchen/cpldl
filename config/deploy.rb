e1d44de2ead8a828f4c70060f95b7462dff0c75b
Ian Knox <knoxid@gmail.com>
2018-03-05 16:00:19 -0700

    [TOOL] update ruby to 2.2.9

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ require 'colorize'
 set :repo_url, "git@github.com:PublicLibraryAssoc/cpldl.git"
 
 # Ruby Version (used for RVM) - MUST MATCH PASSENGER/SERVER CONFIG!
-set :ruby_version, '2.2.3'
+set :ruby_version, '2.2.9'
 
 # Application name (default to using "railsapp")
 #   -- determines deploy path and user
