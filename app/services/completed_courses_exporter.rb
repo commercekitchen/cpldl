e1abc86c74df15c1eaca0fd1056abc7c42e13fbc
Ian Knox <knoxid@gmail.com>
2018-09-18 13:21:48 -0600

    [CHG] add branch to completed courses report

--- app/services/completed_courses_exporter.rb
+++ app/services/completed_courses_exporter.rb
@@ -9,13 +9,13 @@ class CompletedCoursesExporter
   def to_csv
     users = User.includes(:roles).where(organization_id: @org).order(:email)
     CSV.generate do |csv|
-      csv << ["Email", "Program Name", "Course", "Course Completed At"]
+      csv << ["Email", "Program Name", "Course", "Course Completed At", "Branch"]
       users.each do |user|
         if user.reportable_role?(@org)
           user.course_progresses.each do |cp|
             if cp.complete?
               program_name = user.program.present? ? user.program.program_name : ""
-              values = [user.email, program_name, cp.course.title, cp.completed_at.strftime("%m-%d-%Y")]
+              values = [user.email, program_name, cp.course.title, cp.completed_at.strftime("%m-%d-%Y"), user.profile.library_location.try(:name)]
               csv.add_row values
             end
           end
