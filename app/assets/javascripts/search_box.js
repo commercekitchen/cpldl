02d819d7d75ac9c297a576133294957f87f77aea
Tom Reis <tom@commercekitchen.com>
2018-12-10 15:42:20 -0700

    Fix search box interaction

--- app/assets/javascripts/search_box.js
+++ app/assets/javascripts/search_box.js
@@ -5,8 +5,21 @@ $(document).ready(function() {
   });
 
   $("#search").blur(function(event) {
-    $("#search_btn").hide();
-    $(".icon-search").fadeIn(300);
+    if (!$(this).data("no-blur")) {
+      $("#search_btn").hide();
+      $(".icon-search").fadeIn(300);
+    }
+  });
+
+  $("#search_btn").mousedown(function(event) {
+    $("#search").data("no-blur", true);
+  });
+
+  $(document).mouseup(function(event) {
+    if ($("#search").data("no-blur")) {
+      $("#search").data("no-blur", false);
+      $("#search").trigger("blur");
+    }
   });
 
   $(".icon-search").click(function() {
