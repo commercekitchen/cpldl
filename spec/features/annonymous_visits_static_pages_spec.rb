fbe305be27a071dfd9774eb1c1295f98059c1852
mingca <mingca819@gmail.com>
2019-03-06 19:46:48 -0700

    [CHG] add Tools and Resources for Trainers link on header

--- spec/features/annonymous_visits_static_pages_spec.rb
+++ spec/features/annonymous_visits_static_pages_spec.rb
@@ -22,4 +22,31 @@ feature "Annonymous visits static pages" do
     visit cms_page_path(page)
     expect(current_path).to eq(cms_page_path(page))
   end
+
+  describe "Header" do
+    let!(:language) { create(:language) }
+    let!(:spanish_lang) { create(:spanish_lang) }
+
+    shared_examples "trainer link" do
+      it 'should exist on landing page' do
+        visit root_path
+        expect(page).to have_content("Tools and Resources for Trainers")
+      end
+    end
+
+    context "under main domain" do
+      before do
+        switch_to_main_domain
+      end
+      it_behaves_like "trainer link"
+    end
+
+    context "under sub domain" do
+      let(:dpl) { create(:organization, subdomain: "dpl", name: "Denver Public Library") }
+      before do
+        switch_to_subdomain(dpl.subdomain)
+      end
+      it_behaves_like "trainer link"
+    end
+  end
 end
