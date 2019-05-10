2c2cfbc14c05fb705ec34686b88eb5b241b14de2
mingca <mingca819@gmail.com>
2019-05-09 20:11:09 -0600

    [BUG] ruby 2.2.9 no support for safe navigation operator

--- db/migrate/20190509152342_add_organization_id_to_course.rb
+++ db/migrate/20190509152342_add_organization_id_to_course.rb
@@ -2,7 +2,8 @@ class AddOrganizationIdToCourse < ActiveRecord::Migration
   def change
     add_reference :courses, :organization, index: true
     OrganizationCourse.find_each do |org_course|
-      org_course.course&.update(organization: org_course.organization)
+      next if org_course.course.nil?
+      org_course.course.update(organization: org_course.organization)
     end
   end
 end
