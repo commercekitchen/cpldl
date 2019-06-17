6341144b73a656f99e913d7446ff31d91c4a702f
mingca <mingca819@gmail.com>
2019-06-16 20:56:48 -0600

    Footer logo validation duplicate message fix

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -32,8 +32,9 @@ class Organization < ActiveRecord::Base
 
   has_attached_file :footer_logo
 
-  validates_attachment_content_type :footer_logo, content_type: ["image/png", "image/jpeg"], message: ", Only Image files are allowed."
+  validates_attachment_content_type :footer_logo, content_type: ["image/png", "image/jpeg"], message: "should be png or jpeg format."
   validates :footer_logo_link, url: { allow_blank: true }
+  after_validation :clean_up_paperclip_errors
 
   def user_count
     users.count
@@ -65,5 +66,10 @@ class Organization < ActiveRecord::Base
     else
       :password
     end
+  end
+
+  # https://github.com/thoughtbot/paperclip/commit/2aeb491fa79df886a39c35911603fad053a201c0
+  def clean_up_paperclip_errors
+    errors.delete(:footer_logo_content_type)
   end
 end
