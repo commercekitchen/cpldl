b821db9f5de82cd054f4d1251c88426bc6c1dd57
Tom Reis <tom@commercekitchen.com>
2019-09-29 18:23:03 -0600

    Remove deprecated unnamed argument

--- spec/factories/schools.rb
+++ spec/factories/schools.rb
@@ -12,7 +12,7 @@
 
 FactoryBot.define do
   factory :school do
-    school_name { Faker::Hipster.sentence(3) }
+    school_name { Faker::Hipster.sentence(word_count: 3) }
     organization
 
     trait :disabled do
