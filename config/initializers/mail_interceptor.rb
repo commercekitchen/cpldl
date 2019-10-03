2e935117b6616b170a825253e3f3b104b0146fc6
Tom Reis <tom@commercekitchen.com>
2019-10-03 11:37:30 -0600

    Add Tom to mail interceptor whitelist

--- config/initializers/mail_interceptor.rb
+++ config/initializers/mail_interceptor.rb
@@ -2,7 +2,8 @@ unless Rails.env.test? || Rails.env.production?
   options = { forward_emails_to:
     ["joe+stagingdl@ckdtech.co",
       "susie+stagingdl@ckdtech.co",
-      "ming+stagingdl@ckdtech.co"] }
+      "ming+stagingdl@ckdtech.co",
+      "tom+stagingdl@ckdtech.co"] }
   interceptor = MailInterceptor::Interceptor.new(options)
   ActionMailer::Base.register_interceptor(interceptor)
 end
