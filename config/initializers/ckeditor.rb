ffb935280e84bf3bd3fc6f25040a7e8a333b2fea
Tom Reis <tom@commercekitchen.com>
2019-10-03 12:11:21 -0600

    Fix ckeditor

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -40,7 +40,7 @@ Ckeditor.setup do |config|
   # CKEditor CDN
   # More info here http://cdn.ckeditor.com/
   # By default: nil (CDN disabled)
-  config.cdn_url = "//cdn.ckeditor.com/4.13.1/standard/ckeditor.js"
+  config.cdn_url = "//cdn.ckeditor.com/4.6.1/basic/ckeditor.js"
 
   # JS config url
   # Used when CKEditor CDN enabled
