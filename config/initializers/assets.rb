8111d7d9899b8af25b9086e786303eddc0f53375
Tom Reis <tom@commercekitchen.com>
2019-10-16 22:47:46 -0600

    Update asset config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,18 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+Rails.application.config.assets.precompile += %w( admin/courses.js )
+Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg)
+Rails.application.config.assets.paths << "#{Rails.root}/app/assets/fonts"
+Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
+Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
+  Rails.application.config.assets.paths << path
+end
+
+Dir.glob("#{Rails.root}/app/assets/javascripts/**/").each do |path|
+  Rails.application.config.assets.paths << path
+end
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
