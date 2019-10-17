8e053b7e682815a80e48ce6f8118ebe98f0e0af8
Tom Reis <tom@commercekitchen.com>
2019-10-16 22:57:04 -0600

    Don't attempt to precompile node modules

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( admin/courses.js )
 Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg)
