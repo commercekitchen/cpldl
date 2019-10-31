70a5c3f8a746f13c1e0513750fae3e59d26fffbd
Tom Reis <tom@commercekitchen.com>
2019-10-31 12:07:05 -0600

    Update staging config to mimic production

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = true
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -40,8 +40,6 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.assets.paths << "#{Rails.root}/app/assets/fonts"
-  config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
