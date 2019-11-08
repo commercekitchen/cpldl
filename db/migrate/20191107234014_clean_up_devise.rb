f82faf178c0827c6fc8eca0040b49784f991e48c
Tom Reis <tom@commercekitchen.com>
2019-11-08 15:44:14 -0700

    Fix old migration and regenerate schema

--- db/migrate/20191107234014_clean_up_devise.rb
+++ db/migrate/20191107234014_clean_up_devise.rb
@@ -1,12 +1,12 @@
 class CleanUpDevise < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :confirmation_token
-    remove_column :users, :confirmed_at
-    remove_column :users, :confirmation_sent_at
-    remove_column :users, :unconfirmed_email
+    remove_column :users, :confirmation_token, :string
+    remove_column :users, :confirmed_at, :datetime
+    remove_column :users, :confirmation_sent_at, :datetime
+    remove_column :users, :unconfirmed_email, :string
 
-    remove_column :users, :failed_attempts
-    remove_column :users, :unlock_token
-    remove_column :users, :locked_at
+    remove_column :users, :failed_attempts, :integer, default: 0, null: false
+    remove_column :users, :unlock_token, :string
+    remove_column :users, :locked_at, :datetime
   end
 end
