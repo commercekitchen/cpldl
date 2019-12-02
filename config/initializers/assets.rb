bcc4e23e59b946403df2fada39ddbe4479aec2f1
Tom Reis <tom@ckdtech.co>
2019-12-02 14:00:10 -0700

    Precompile pdf stylesheet

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,8 +8,8 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 # Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.precompile += %w( admin/courses.js )
-Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg)
+Rails.application.config.assets.precompile += %w( admin/courses.js pdf.css )
+Rails.application.config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif *.svg )
 Rails.application.config.assets.paths << "#{Rails.root}/app/assets/fonts"
 Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
 Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
