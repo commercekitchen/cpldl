e269ef3521fec4d56000717add0a97090110533e
Tom Reis <tmichaelreis@gmail.com>
2020-07-03 18:15:14 -0600

    Add school info to registrations report

--- spec/factories/schools.rb
+++ spec/factories/schools.rb
@@ -3,6 +3,7 @@
 FactoryBot.define do
   factory :school do
     school_name { Faker::Hipster.sentence(word_count: 3) }
+    school_type { :middle_school }
     organization
 
     trait :disabled do
--- spec/services/registration_exporter_spec.rb
+++ spec/services/registration_exporter_spec.rb
@@ -5,16 +5,12 @@ require 'csv'
 
 describe RegistrationExporter do
   describe 'email user' do
-    let(:organization) { FactoryBot.create(:organization) }
-    let(:branch_organization) { FactoryBot.create(:organization, branches: true) }
-    let(:branch) { FactoryBot.create(:library_location, organization: branch_organization) }
-    let(:program) { FactoryBot.create(:program, organization: organization) }
-    let!(:user) { FactoryBot.create(:user, organization: organization) }
-    let!(:program_user) { FactoryBot.create(:user, program: program, organization: organization) }
-    let(:profile) { FactoryBot.build(:profile, library_location: branch) }
-    let!(:branch_user) { FactoryBot.create(:user, profile: profile, organization: branch_organization) }
-
     describe 'no branches organization' do
+      let(:organization) { FactoryBot.create(:organization) }
+      let(:program) { FactoryBot.create(:program, organization: organization) }
+      let!(:user) { FactoryBot.create(:user, organization: organization) }
+      let!(:program_user) { FactoryBot.create(:user, program: program, organization: organization) }
+
       let(:subject) { RegistrationExporter.new(organization) }
       let(:parsed_report) { CSV.parse(subject.to_csv, headers: true) }
 
@@ -36,7 +32,12 @@ describe RegistrationExporter do
     end
 
     describe 'branches organization' do
-      let(:subject) { RegistrationExporter.new(branch_organization) }
+      let(:organization) { FactoryBot.create(:organization, branches: true) }
+      let(:branch) { FactoryBot.create(:library_location, organization: organization) }
+      let(:profile) { FactoryBot.build(:profile, library_location: branch) }
+      let!(:branch_user) { FactoryBot.create(:user, profile: profile, organization: organization) }  
+      
+      let(:subject) { RegistrationExporter.new(organization) }
       let(:parsed_report) { CSV.parse(subject.to_csv, headers: true) }
 
       it 'should generate correct column headers' do
@@ -49,6 +50,39 @@ describe RegistrationExporter do
 
       it 'should match branch zipcode' do
         expect(parsed_report.to_s).to match(branch.zipcode.to_s)
+      end
+    end
+
+    describe 'student program organization' do
+      let(:organization) { FactoryBot.create(:organization, accepts_programs: true) }
+      let(:program) { FactoryBot.create(:program, parent_type: :students_and_parents, organization: organization) }
+      let(:school) { FactoryBot.create(:school, organization: organization) }
+      let(:profile) { FactoryBot.build(:profile, :with_last_name) }
+      let(:subject) { RegistrationExporter.new(organization) }
+      let(:parsed_report) { CSV.parse(subject.to_csv, headers: true) }
+
+      before do
+        @user = FactoryBot.create(:user, profile: profile, program: program, organization: organization, school: school, student_id: '12345')
+      end
+
+      it 'should generate correct column headers' do
+        expect(parsed_report.headers).to eq(['Email', 'Program Name', 'Registration Date', 'School Type', 'School Name', 'Student ID(s)'])
+      end
+
+      it 'should include program name' do
+        expect(parsed_report.to_s).to match(program.program_name)
+      end
+
+      it 'should include school type' do
+        expect(parsed_report.to_s).to match(school.school_type.titleize)
+      end
+
+      it 'should include school name' do
+        expect(parsed_report.to_s).to match(school.school_name)
+      end
+
+      it 'should include student IDs value' do
+        expect(parsed_report.to_s).to match(@user.student_id)
       end
     end
   end
