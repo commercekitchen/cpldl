38007735d291afe197e214c40ae2a7519f2fd8ed
Tom Reis <tmichaelreis@gmail.com>
2020-07-16 14:16:10 -0600

    Only list active schools in registration dropdown

--- spec/controllers/ajax/schools_controller_spec.rb
+++ spec/controllers/ajax/schools_controller_spec.rb
@@ -11,21 +11,26 @@ describe Ajax::SchoolsController do
     @middle_school = create(:school, school_type: :middle_school, organization: organization)
     @high_school = create(:school, school_type: :high_school, organization: organization)
 
+    create(:school, school_type: :elementary, organization: organization, enabled: false)
+    create(:school, school_type: :elementary, organization: organization, enabled: false)
+    create(:school, school_type: :middle_school, organization: organization, enabled: false)
+    create(:school, school_type: :high_school, organization: organization, enabled: false)
+
     @request.host = "#{organization.subdomain}.test.host"
   end
 
   describe 'GET #index' do
-    it 'returns elementary schools' do
+    it 'returns active elementary schools' do
       get :index, params: { school_type: 'elementary' }, format: :js
       expect(JSON.parse(response.body)).to contain_exactly(JSON.parse(@elementary_school1.to_json), JSON.parse(@elementary_school2.to_json))
     end
 
-    it 'returns middle schools' do
+    it 'returns active middle schools' do
       get :index, params: { school_type: 'middle_school' }, format: :js
       expect(response.body).to eq([@middle_school].to_json)
     end
 
-    it 'returns high schools' do
+    it 'returns active high schools' do
       get :index, params: { school_type: 'high_school' }, format: :js
       expect(response.body).to eq([@high_school].to_json)
     end
