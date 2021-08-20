c39726114681f8ee9f4bce3b698443b85975ec08
Tom Reis <tom@ckdtech.co>
2021-08-19 20:18:40 -0600

    Hide course intro text when no search results are found

--- spec/features/anonymous_user/course_search_spec.rb
+++ spec/features/anonymous_user/course_search_spec.rb
@@ -1,23 +1,19 @@
-# frozen_string_literal: true
-
 require 'feature_helper'
 
 feature 'User searches for courses' do
   let(:org) { FactoryBot.create(:organization) }
-  let(:user) { FactoryBot.create(:user, organization: org) }
   let!(:course) { FactoryBot.create(:course, organization: org) }
 
   before do
     switch_to_subdomain(org.subdomain)
-    login_as(user)
     visit root_path
   end
 
   scenario 'no courses found' do
     fill_in 'Search', with: 'foobar'
     click_on('submit-search')
-    expect(page).to have_content(I18n.t('logged_in_user.hi'))
-    expect(page).not_to have_content(I18n.t("logged_in_user.choose_a_course"))
+    expect(page).not_to have_content(I18n.t("home.choose_a_course.#{org.subdomain}"))
+    expect(page).not_to have_content(I18n.t("home.choose_course_subheader.#{org.subdomain}"))
     expect(page).to have_content('No courses match your search.')
     expect(page).to have_link('View all courses', href: courses_path)
   end
