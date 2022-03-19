85148291c95ce9364bb490aadd96b6cef28c7bed
Tom Reis <tom@ckdtech.co>
2022-03-19 17:32:39 -0600

    Create partners for Orange County subsite

--- db/data/20220313161911_create_ocnc_subsite.rb
+++ db/data/20220313161911_create_ocnc_subsite.rb
@@ -6,11 +6,16 @@ class CreateOcncSubsite < ActiveRecord::Migration[5.2]
       subdomain: 'ocnc',
       branches: false,
       accepts_programs: false,
-      accepts_partners: false
+      accepts_partners: true
     }
 
     # Create the subdomain organization
     subsite = Organization.create!(subsite_attributes)
+
+    # Create partners
+    ['Orange County Public Library', 'CWS', 'Other'].each do |partner|
+      subsite.partners.create!(name: partner)
+    end
 
     # Import all subsite courses
     Course.pla.where(pub_status: 'P').each do |course|
