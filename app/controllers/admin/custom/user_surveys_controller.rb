0918520c0391742b68bd6d2f76d0b440c27e6f38
Tom Reis <tom@ckdtech.co>
2022-04-29 14:50:52 -0600

    Add spanish survey link option to surveys form

--- app/controllers/admin/custom/user_surveys_controller.rb
+++ app/controllers/admin/custom/user_surveys_controller.rb
@@ -27,14 +27,18 @@ module Admin
       end
 
       def org_params
-        params.require(:organization).permit(:user_survey_link, :user_survey_button_text, :user_survey_enabled)
+        params.require(:organization).permit(:user_survey_link,
+                                             :spanish_survey_link,
+                                             :user_survey_button_text,
+                                             :user_survey_enabled)
       end
 
       def update_translations
         @translation_errors = []
         params.require(:translation).permit!.each do |_locale, values|
           translation = Translation.find_or_initialize_by(key: values[:key], locale: values[:locale])
-          unless translation.update(values)
+
+          unless translation.update(values.except(:id))
             @translation_errors << translation.errors.full_messages
             return false
           end
