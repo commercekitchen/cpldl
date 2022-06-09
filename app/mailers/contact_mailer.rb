c5ade4431f8590cca29d0d7be23e8cc98ca3fd6a
Tom Reis <tom@ckdtech.co>
2022-06-09 11:55:28 -0600

    Send contact email to new address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@
 class ContactMailer < ApplicationMailer
 
   def email(contact_id)
-    to = 'sallen@ala.org'
+    to = 'pla@ala.org'
     subject = 'New DigitalLearn.org Contact Form Submitted'
     @contact = Contact.find(contact_id)
     mail(to: to, subject: subject)
