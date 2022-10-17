3c82edd884f7e37ffe83735dbbd6dd8aabfad9ef
Tom Reis <tom@ckdtech.co>
2022-10-16 23:03:46 -0600

    Allow course attachment reordering

--- app/controllers/admin/attachments_controller.rb
+++ app/controllers/admin/attachments_controller.rb
@@ -8,5 +8,15 @@ module Admin
       @attachment.destroy
       redirect_to edit_admin_course_path(@attachment.course)
     end
+
+    def sort
+      authorized_courses = policy_scope(Course)
+      attachments = Attachment.where(course: authorized_courses)
+      SortService.sort(model: attachments, order_params: params[:order], attribute_key: :attachment_order, user: current_user)
+
+      head :ok
+    rescue ActiveRecord::RecordNotFound
+      raise Pundit::NotAuthorizedError
+    end
   end
 end
