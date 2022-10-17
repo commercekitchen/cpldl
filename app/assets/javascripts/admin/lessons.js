eb43a6e4d84e0a058f8475c43b62994ca45110c4
Tom Reis <tom@ckdtech.co>
2022-10-16 21:04:27 -0600

    Update lesson SEO Page Title with lesson title

--- app/assets/javascripts/admin/lessons.js
+++ app/assets/javascripts/admin/lessons.js
@@ -1,9 +1,13 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
   // If the seo title is empty, prepopulate with title value.
-  $("body").on("blur", "#lesson_title", function() {
-    if($("#lesson_seo_page_title").val().trim() === "") {
+  $("body").on("blur", "#lesson_title", function () {
+    if ($("#lesson_seo_page_title").val().trim() === "") {
       $("#lesson_seo_page_title").val($("#lesson_title").val());
     }
+  });
+
+  $("#lesson_title").change(function () {
+    var value = $(this).val();
+    $("#lesson_seo_page_title").val(value);
   });
 });
