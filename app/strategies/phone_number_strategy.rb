054c02cea096b06f883386565fd1af2c2974f31c
Tom Reis <tom@ckdtech.co>
2023-01-22 20:40:59 -0700

    Add user role for phone number users

--- app/strategies/phone_number_strategy.rb
+++ app/strategies/phone_number_strategy.rb
@@ -11,6 +11,7 @@ class PhoneNumberStrategy < Warden::Strategies::Base
     user = User.find_or_create_by(phone_number: phone_number, organization: current_organization)
 
     if user.valid?
+      user.add_role(:user, current_organization)
       success!(user)
     else
       fail!(user.errors.full_messages.join(','))
