17c859457c40697fab5d3a501454457f6c595946
Tom Reis <tom@ckdtech.co>
2023-05-05 12:46:43 -0600

    Try port 465 for email

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -12,7 +12,7 @@ else
     :password => api_key,
     :domain => domain,
     :address => 'smtp.sendgrid.net',
-    :port => 25,
+    :port => 465,
     :authentication => :plain,
     :enable_starttls_auto => true
   }
