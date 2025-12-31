2a3f42c4b0921b4d657cbf7d95a3665a62328a29
Tom Reis <tom@showami.com>
2025-12-31 09:04:20 -0700

    Use unique log group name

--- ops/modules/sidekiq/log_groups.tf
+++ ops/modules/sidekiq/log_groups.tf
@@ -1,4 +1,8 @@
 resource "aws_cloudwatch_log_group" "sidekiq_log_group" {
-  name              = "/ecs/sidekiq-${var.environment_name}"
+  name              = "${var.project_name}/${var.environment_name}/ecs/sidekiq"
   retention_in_days = var.log_retention_days
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
