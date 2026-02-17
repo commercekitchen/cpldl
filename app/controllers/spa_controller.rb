da284b0cf3e2aae1827c51d9a207d464eaace9e6
Tom Reis <tom@showami.com>
2026-02-17 13:06:53 -0700

    [BUG] Fix subdomain redirects for spa

--- app/controllers/spa_controller.rb
+++ app/controllers/spa_controller.rb
@@ -1,5 +1,9 @@
-class SpaController < ActionController::Base
+# frozen_string_literal: true
+
+class SpaController < ApplicationController
   def index
+    skip_authorization
+    skip_policy_scope
     render :index, layout: 'spa'
   end
 end
